e8daeba9da7c91503ebd0b48654e1cb309d062f8
Jan Baudisch <dev@baudisch.xyz>
2018-08-11 11:02:33 +0200

    add example

---
+++ examples/vplan.rs
@@ -0,0 +1,95 @@
+extern crate chrono;
+extern crate futures;
+extern crate hyper;
+extern crate libvplan;
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate structopt;
+
+use chrono::{Datelike, Weekday};
+use futures::Future;
+use libvplan::{Client, WeekType};
+use std::process;
+use structopt::StructOpt;
+
+macro_rules! errorexit {
+    ($($error:tt)*) => {{
+        error!($($error)*);
+        process::exit(1);
+    }}
+}
+
+/// libvplan CLI tool
+#[derive(StructOpt, Debug)]
+#[structopt(name = "vplan")]
+struct Cli {
+    /// Login username
+    #[structopt(short = "u", long = "username")]
+    username: String,
+    /// Login password
+    #[structopt(short = "p", long = "password")]
+    password: String,
+    /// Weekday to fetch vplan for
+    #[structopt(name = "weekday", parse(from_str))]
+    weekday: String
+}
+
+fn main() {
+    let cli = Cli::from_args();
+
+    pretty_env_logger::init();
+
+    let weekday = match cli.weekday.to_lowercase().as_ref() {
+        "monday" => Weekday::Mon,
+        "tuesday" => Weekday::Tue,
+        "wednesday" => Weekday::Wed,
+        "thursday" => Weekday::Thu,
+        "friday" => Weekday::Fri,
+        _ => errorexit!("invalid weekday")
+    };
+
+    let client = Client::new(cli.username.as_ref(), cli.password.as_ref());
+
+    let future = client
+        .get_vplan(weekday)
+        .map(|vplan| {
+            let date = vplan.date.date;
+
+            let week_type = match vplan.date.week_type {
+                WeekType::A => "A",
+                WeekType::B => "B"
+            };
+
+            println!(
+                "{}.{}.{} ({}-Woche)\n",
+                date.day(),
+                date.month(),
+                date.year(),
+                week_type
+            );
+
+            for change in vplan.changes {
+                println!(
+                    "{}: {} {} {} {} {}",
+                    change.class,
+                    change.lesson,
+                    change.subject,
+                    change.teacher,
+                    change.room,
+                    change.info
+                );
+            }
+
+            for info in vplan.info {
+                println!("{}\n", info);
+            }
+
+            process::exit(0);
+        }).map_err(|error| {
+            errorexit!("{}", error);
+        });
+
+    hyper::rt::run(future);
+}
